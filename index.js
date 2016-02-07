9f184d3b2a28dda33f2521fd05356baa0e1df9ef
Jason Schapiro <yono38@gmail.com>
2016-02-07 16:48:06 -0500

    Changes to dynamically grab bearer token for my fitness pal

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').load();
 var eq = require('./lib/equinox.js')();
 var fp = require('./lib/myfitnesspal.js')();
 var duo = require('./lib/duolingo.js')();
+var mint= require('./lib/mint.js')();
 var express = require('express'),
     cors = require('cors');
 var Promise = require('bluebird');
@@ -25,19 +26,26 @@ app.get('/eq', function (req, res) {
   });
 });
 
+app.get('/mint', function(req, res) {
+  mint.loginAndGetTransactions().then(function(data) {
+    res.json(data);
+  });
+});
+
 app.get('/fp', function(req, res) {
-  fp.getCalorieInfo().then(function(data) {
+  fp.loginAndGetCalorieInfo().then(function(data) {
     res.json(data);
   });
 });
 
 app.get('/trackall', function(req, res) {
-  Promise.all([fp.getCalorieInfo(), eq.loginAndGetCheckins(), duo.loginAndGetStreak()]).then(function(resolved) {
+  Promise.all([fp.loginAndGetCalorieInfo(), eq.loginAndGetCheckins(), duo.loginAndGetStreak(), mint.loginAndGetTransactions()]).then(function(resolved) {
     console.log(resolved);
     var resObj = {
       calories: resolved[0],
       gym: resolved[1],
-      language: resolved[2]
+      language: resolved[2],
+      money: resolved[3]
     };
 
     res.json(resObj);
