419e79936512d64a984ecff3eb58bcde12338bd0
Jason Schapiro <yono38@gmail.com>
2016-02-08 23:38:30 -0500

    Adding bar chart of spending

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,8 +1,78 @@
 $(document).ready(function() {
+  function makeBarChart(txns) {
+    var barData = _.map(txns, function(val, key) {
+      return {
+        'x': moment(key, 'MMM D').format('M/D'),
+        'y': val
+      };
+    }).slice(0, 7).reverse();
+
+  var vis = d3.select('#visualisation'),
+    WIDTH = 275,
+    HEIGHT = 175,
+    MARGINS = {
+      top: 5,
+      right: 5,
+      bottom: 5,
+      left: 5
+    },
+    xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function (d) {
+      return d.x;
+    })),
+
+
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
+      d3.max(barData, function (d) {
+        return d.y;
+      })
+    ]),
+
+    xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true),
+
+    yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient("left")
+      .tickSubdivide(true);
+
+
+  vis.append('svg:g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+    .call(xAxis);
+
+  vis.append('svg:g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+    .call(yAxis);
+
+  vis.selectAll('rect')
+    .data(barData)
+    .enter()
+    .append('rect')
+    .attr('x', function (d) {
+      return xRange(d.x);
+    })
+    .attr('y', function (d) {
+      return yRange(d.y);
+    })
+    .attr('width', xRange.rangeBand())
+    .attr('height', function (d) {
+      return ((HEIGHT - MARGINS.bottom) - yRange(d.y));
+    })
+    .attr('fill', '#F4F4F9');
+
+}
+
+
   function makeTemplateData(trackerData) {
     var tileData = [
       { // Gym
         imgUrl: 'http://www.equinox.com/assets/images/sharing/equinox_logo.jpg',
+        type: 'gym',
         icon: 'gym8',
         result: trackerData.gym.totals.month,
         goal: undefined,
@@ -10,6 +80,7 @@ $(document).ready(function() {
       },
       { // Food
         imgUrl: 'https://upload.wikimedia.org/wikipedia/en/6/63/MyFitnessPal_Logo.png',
+        type: 'food',
         icon: 'restaurant23',
         result: trackerData.calories.netCalories,
         goal: 1601,
@@ -17,6 +88,7 @@ $(document).ready(function() {
       },
        { // Duolingo
         imgUrl: 'http://media.tumblr.com/5fd6b3ccc4e8c978c87f469b236558ad/tumblr_inline_mwkqv1OuOg1ss97ol.png',
+        type: 'chat',
         icon: 'chat27',
         result: trackerData.language.streak,
         goal: undefined,
@@ -24,6 +96,7 @@ $(document).ready(function() {
       },   
       { // Money
         imgUrl: 'http://thefinancialbrand.com/wp-content/uploads/2015/06/Mint-logo.png',
+        type: 'money',
         icon: 'money132',
         result: trackerData.language.streak,
         result: '$' + trackerData.money.recent.total.toFixed(2),
@@ -45,5 +118,9 @@ $(document).ready(function() {
     $('#dash-container').html(
       template(templateData)
     );
+    makeBarChart(trackerData.money.transactions);
+    $( "div[id^='tracker-money']" ).on('click', function() {
+      $( "div[id^='tracker-money']" ).toggleClass('hide-tile');
+    });
   });
 });
